d9055eae4d212368c00c5544c401db4ecd3389c9
tjmehta <tejesh.mehta@gmail.com>
2014-05-03 13:15:23 -0700

    define properly using defineProperty

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-Function.prototype.bindRight = function (thisArg /*, rightArgs... */) {
-  var rightArgs = Array.prototype.slice.call(arguments, 1);
-  var fn = this;
-  return function (/* args.. */) {
-    var args = Array.prototype.slice.call(arguments).concat(rightArgs);
-    return fn.apply(thisArg, args);
-  };
-};
+if (!Function.prototype.bindRight) {
+  Object.defineProperty(Function.prototype, 'bindRight', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function (thisArg /*, rightArgs... */) {
+      var rightArgs = Array.prototype.slice.call(arguments, 1);
+      var fn = this;
+      return function (/* args.. */) {
+        var args = Array.prototype.slice.call(arguments).concat(rightArgs);
+        return fn.apply(thisArg, args);
+      };
+    }
+  });
+}
